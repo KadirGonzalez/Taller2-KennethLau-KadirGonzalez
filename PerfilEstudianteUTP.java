5309254a88753ecf8d51364784e8eb6d7eb7d1f1
KadirGonzalez <kegm0670@gmail.com>
2026-09-07 19:57:11 -0500

    Update PerfilEstudianteUTP.java

--- PerfilEstudianteUTP.java
+++ PerfilEstudianteUTP.java
@@ -1,6 +1,7 @@
 /*
  * TODO 1 (NIVEL 2 - Javadoc): Convierte este comentario de bloque a un comentario Javadoc oficial (/** ... */)
- /** Incluye las etiquetas @author ( Kadir Gonzalez 8-1044-1802 y Kenneth Lau 8-1047-1451) y @version 1.0 
+ /** Incluye las etiquetas @author ( Kadir Gonzalez 8-1044-1802 y Kenneth Lau 8-1047-1451) 
+ @version 1.0 
  */
 public class PerfilEstudianteUTP {
 
